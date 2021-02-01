0053e3e0f745afcdac0da7eb5be02f44928d5fa9
Kushanrawindu <kushanrawindu@gmail.com>
2021-02-01 10:40:36 +0530

    add chat feed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine'
 import './App.css';
+import ChatFeed from './components/ChatFeed'
+// import ChatFeed from './component/ChatFeed'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+        height="100vh"
+        projectID="94a03afc-fee1-4323-a560-a68fce8b8a56"
+        userName="Kushanrawindu"
+        userSecret="963320213"
+        renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}/>}
+    />
+      
+    
   );
 }
 
