2d7d0f403be03a6ce116c6a8bd6c0665db1ab9e4
Kushanrawindu <kushanrawindu@gmail.com>
2021-02-07 23:19:13 +0530

    change chat subtitle color

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,9 +1,10 @@
 import {useState} from 'react'
 import {sendMessage, isTyping} from 'react-chat-engine'
+import {SendOutlined, PictureOutlined} from '@ant-design/icons'
 
 const MessageForm = (props) => {
 
-    const {value, setValue} = useState('');
+    const [value, setValue] = useState('');
     const { chatId, creds} = props;
     
     const handleSubmit = (event) => {
@@ -12,8 +13,16 @@ const MessageForm = (props) => {
         const text = value.trim();
 
         if(text.length > 0) sendMessage(creds, chatId, {text});
+        
+        setValue('');
     }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, {files: event.target.value, text:''})
+    }
+    
     const handleChange = (event) => {
+        
         setValue(event.target.value);
 
         isTyping(props, chatId);
@@ -26,6 +35,21 @@ const MessageForm = (props) => {
             value={value}
             onChange={handleChange}
             onSubmit={handleSubmit}/>
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon"/>
+                </span>
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{display: 'none'}}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon"/>
+            </button>
         </form>
     );
 }
